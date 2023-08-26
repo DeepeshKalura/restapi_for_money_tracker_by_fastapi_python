dcd06eaeb84eaf3ef155bfc96ab9eeed12332b29
DeepeshKalura <deepeshkalurs@gmail.com>
2023-08-26 13:03:07 +0530

    added cors to the api

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from .routes import cors
+
 
 app = FastAPI()
+cors.add_cors(app)
 
-@app.get("/")
+
+
+
+
+@app.get("/expenses", tags=["Expenses"])
+async def get_expenses():
+    return {"message": "Expenses"}
+
+
+
+
+@app.get("/", tags=["Root"])
 async def root():
     return {"message": "Hello World"}   
